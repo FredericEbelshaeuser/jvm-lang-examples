3206f3d1c26ca8a12a491c71dfe3be3c4557205a
Frederic Ebelshaeuser <ebelshaeuser@yatta.de>
2017-05-12 15:35:26 +0200

    Adapt Groovy Example with clojure

--- GroovyExample/src/de/yatta/assemble/groovy/ExampleG.groovy
+++ GroovyExample/src/de/yatta/assemble/groovy/ExampleG.groovy
@@ -1,16 +1,28 @@
 package de.yatta.assemble.groovy
 
-	print gaussifyG(0, 0..100)
-
+	println gaussifyG(0, 0..100)
+	println gaussifyG("", "A".."Z")
+	println "\n"
 	
 	def gaussifyG(a,r){
-		int sum=a
+		//will crash because of char range
+		//		int sum=a
+		def sum=a
 		for(i in r){
 			sum+=i
 		}
 		sum
 	}
 	
+
 	
+	println gaussifyMoreGroovyLike(0, 0..100)
+	println gaussifyMoreGroovyLike("", "A".."Z")
+	
+	def gaussifyMoreGroovyLike(a,r){
+		
+		r.inject(a, {sum,i-> sum+i})
+	
+	}
 	
 
